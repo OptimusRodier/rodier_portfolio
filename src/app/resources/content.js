130d57b87a0c9aa55249cae28c3e88f8f38817fe
OptimusRodier <57970718+OptimusRodier@users.noreply.github.com>
2025-05-09 19:07:01 +0200

    fix 1

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -60,8 +60,8 @@ const home = {
   headline: <>Hello! Rodier here...</>,
   /*featured: {
     display: true,
-    /*title: <>Recent project: <strong className="ml-4">Once UI</strong></>,
-    href: "/work/building-once-ui-a-customizable-design-system",
+    title: <>Recent project: <strong className="ml-4">Chezyo</strong></>,
+    href: "https://chezyo.com", // You can link to a case study or external site
   },*/
   subline: (
     <>
